dc0176e46820d62bb207015fb8984f03a7eb9ffd
mizok <mizok.huang@neutec.com.tw>
2023-10-27 00:08:47 +0800

    fix:修正日誌刪除功能

--- .git_commands_logger.sh
+++ .git_commands_logger.sh
@@ -17,12 +17,78 @@ function record_git_commands {
     fi
 }
 
+function delete_outdated_logs {
+    # log保存數量上限
+    local max_log_cache=30
+    # log保存的天數上限
+    local max_log_preserve_day=7
+    # 檢查並刪除超過n天的日誌文件
+    local latest_log=$(ls -t "$log_directory/${repo_name}_git_commands_"*.log 2>/dev/null | head -1)
+    # 從檔案路徑中提取日期部分（假設日期部分的格式固定）
+    local date_part=$(echo "$latest_log" | grep -oE '[0-9]{14}')
+    local date_current=$(date "+%Y%m%d%H%M%S")
+    # 將日期轉換為Unix時間戳記（以秒為單位）
+    local latest_log_timestamp=$(date -j -f "%Y%m%d%H%M%S" "$date_part" "+%s")
+    local current_timestamp=$(date -j -f "%Y%m%d%H%M%S" "$date_current" "+%s")
+    local days_difference=$(((current_timestamp - latest_log_timestamp) / 86400)) # 86400 seconds in a day
+
+    if [ "$days_difference" -gt $max_log_preserve_day ]; then
+        echo -e "\e[33此專案的日誌皆已過期 $max_log_preserve_day 天以上，請問是否刪除所有日誌檔案？ (y/n):\e[0m"
+        read userInput
+
+        if [[ "$userInput" == "y" ]]; then
+            rm "$log_directory/${repo_name}_git_commands_"*.log
+            echo
+            echo "已刪除所有過期的日誌檔案。"
+            echo
+        else
+            echo
+            echo "取消刪除。"
+            echo
+        fi
+    fi
+
+    # 檢查日誌檔案是否超過n篇, 若超過則刪除一半的日誌文件
+    logs_count=$(ls -1 "$log_directory/${repo_name}_git_commands_"*.log 2>/dev/null | wc -l)
+    if [ "$logs_count" -gt $max_log_cache ]; then
+        local num_logs_to_keep=0
+        if [[ $(($max_log_cache % 2)) -eq 1 ]]; then
+            num_logs_to_keep=$((($max_log_cache - 1) / 2))
+        else
+            num_logs_to_keep=$(($max_log_cache / 2))
+        fi
+
+        local num_logs_to_delete=$((logs_count - num_logs_to_keep))
+
+        local oldest_logs=$(ls -t "$log_directory/${repo_name}_git_commands_"*.log 2>/dev/null | tail -$num_logs_to_delete)
+        local oldest_logs_arr=("$=oldest_logs")
+
+        echo -e "\e[33m即將刪除最舊的 $num_logs_to_delete 篇日誌：\e[0m"
+        echo -e "\e[37m$oldest_logs\e[0m"
+        echo
+        echo -e "\e[47m\e[33m是否確定刪除? (y/n):\e[0m\e[49m "
+        read userInput
+
+        if [[ "$userInput" == "y" ]]; then
+            for log_file in "${oldest_logs_arr[@]}"; do
+                rm "$log_file"
+            done
+            echo
+            echo "已刪除指定的日誌檔案。"
+            echo
+        else
+            echo
+            echo "取消刪除。"
+            echo
+        fi
+    fi
+
+}
+
 function setup_git_commands_logging {
-    local timestamp=$(date "+%Y:%m:%d:%H:%M:%S")
+    local timestamp=$(date "+%Y/%m/%d %H:%M:%S")
     initial=0
     current_command=""
-    max_log_cache=4
-    max_log_preserve_day=7
 
     function my_preexec() {
         # 在執行命令之前執行的程式碼
@@ -33,7 +99,7 @@ function setup_git_commands_logging {
     # 取得Git倉庫的根目錄名
     if [ -d .git ] || git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
         repo_name=$(basename "$(git rev-parse --show-toplevel)")
-        log_directory="/Users/mizokhuangmbp2023/.git_command_log/$repo_name"
+        log_directory="$HOME/.git_commands_logger/logs/$repo_name"
 
         # 如果日誌資料夾不存在，則建立它
         if [ ! -d "$log_directory" ]; then
@@ -50,6 +116,9 @@ function setup_git_commands_logging {
         echo " " >>"$log_file"
         echo "\e[42m\e[30m已經打開 $repo_name 專案, 開始即時記錄git的相關操作\e[0m\e[49m"
 
+        #刪除過期的log
+        delete_outdated_logs
+
         # 自訂鉤子，使得每次指令結束後都會呼叫my_preexec函數
         autoload -U add-zsh-hook
         add-zsh-hook preexec my_preexec
@@ -63,58 +132,6 @@ function setup_git_commands_logging {
             fi
 
         }
-
-        # 檢查並刪除超過7天的日誌文件
-        latest_log=$(ls -t "$log_directory/${repo_name}_git_commands_"*.log 2>/dev/null | head -1)
-        # 從檔案路徑中提取日期部分（假設日期部分的格式固定）
-        date_part=$(echo "$latest_log" | grep -oE '[0-9]{14}')
-        # 將日期轉換為Unix時間戳記（以秒為單位）
-        unix_timestamp=$(date -j -f "%Y%m%d%H%M%S" "$date_part" "+%s")
-        current_timestamp=$(date "+%Y%m%d%H%M%S")
-        days_difference=$(((current_timestamp - latest_log_timestamp) / 86400)) # 86400 seconds in a day
-        echo $current_timestamp
-        echo $latest_log
-
-        if [ "$days_difference" -gt $max_log_preserve_day ]; then
-            echo -e "\e[33m日誌已超過 $max_log_preserve_day 天，即將刪除全部日誌。\e[0m"
-            echo -e "\e[47m\e[33m是否確定刪除? (y/n):\e[0m\e[49m "
-            read userInput
-
-            if [[ "$userInput" == "y" ]]; then
-                rm "$log_directory/${repo_name}_git_commands_"*.log
-                echo
-                echo "檔案已刪除。"
-                echo
-            else
-                echo
-                echo "取消刪除。"
-                echo
-            fi
-        fi
-
-        # 檢查並刪除超過50篇的五成日誌文件
-        logs_count=$(ls -1 "$log_directory/${repo_name}_git_commands_"*.log 2>/dev/null | wc -l)
-        if [ "$logs_count" -gt $max_log_cache ]; then
-            num_logs_to_delete=$((logs_count / 2)) # Delete 50% of the logs
-            oldest_logs=$(ls -t "$log_directory/${repo_name}_git_commands_"*.log 2>/dev/null | tail -$num_logs_to_delete)
-
-            echo -e "\e[33m即將刪除最舊的 $num_logs_to_delete 篇日誌：\e[0m"
-            echo -e "\e[37m$oldest_logs\e[0m"
-            echo
-            echo -e "\e[47m\e[33m是否確定刪除? (y/n):\e[0m\e[49m "
-            read userInput
-
-            if [[ "$userInput" == "y" ]]; then
-                rm $oldest_logs
-                echo
-                echo "檔案已刪除。"
-                echo
-            else
-                echo
-                echo "取消刪除。"
-                echo
-            fi
-        fi
 
     fi
 }
