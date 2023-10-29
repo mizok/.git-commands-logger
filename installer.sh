2c16165cca6e218d86a9a12e138096a1c23361f3
mizok <mizok.huang@neutec.com.tw>
2023-10-30 00:00:20 +0800

    update:install.sh

--- installer.sh
+++ installer.sh
@@ -8,18 +8,24 @@ source $gitCommandLogger'
 # 下載 .git_commands_logger.sh 文件
 git_commands_logger_url="https://raw.githubusercontent.com/mizok/.git-commands-logger/main/.git_commands_logger.sh"
 download_path="$root_path/.git_commands_logger/.git_commands_logger.sh"
-if [  -f "$download_path" ]; then
+if [ -f "$download_path" ]; then
     rm -f $download_path
+else
+    curl -fsSL -o "$download_path" "$git_commands_logger_url"
+    if [ $? -eq 0 ]; then
+        echo "腳本已成功下載到您的主目錄。"
+        chmod +x $download_path # 賦予腳本執行權限（如果需要）
+    else
+        echo "無法下載腳本。請檢查網絡連接和URL。"
+    fi
 fi
-curl -fsSL -o "$download_path" "$git_commands_logger_url"
-chmod +x $download_path
 
 # 檢查.zshrc文件是否存在
 zshrc_path="$root_path/.zshrc"
 if [ -f "$zshrc_path" ]; then
     # 檢查是否已經存在相應的設定，如果不存在，則添加到.zshrc文件中
     if ! grep -qF "$config_lines" "$zshrc_path"; then
-        echo -e "\n# Add Git Command Logger configuration\n$config_lines" >> "$zshrc_path"
+        echo -e "\n# Add Git Command Logger configuration\n$config_lines" >>"$zshrc_path"
         echo "Configuration added to $zshrc_path"
     else
         echo "Configuration already exists in $zshrc_path"
