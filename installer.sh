38486f344e3fe1a50481c8af949f9980bb48b1ba
mizok <mizok.huang@neutec.com.tw>
2023-11-06 23:50:12 +0800

    fix: 修正echo 提示錯誤

--- installer.sh
+++ installer.sh
@@ -57,7 +57,7 @@ echo "\033[32m請問是否要設定全域Git hooks偵測功能 (y/n)?\033[0m"
 read userInput1
 if [[ $userInput1 == 'y' ]]; then
     echo
-    echo "\033[32m此設定將會改寫git的全域屬性core.hook , 是否確認這個改動 (y/n)?\033[0m"
+    echo "\033[32m此設定將會改寫git的全域屬性core.hooksPath , 是否確認這個改動 (y/n)?\033[0m"
     read userInput2
     if [[ $userInput2 == 'y' ]]; then
         set_global_hooks
